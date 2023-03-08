bc0d56e346a9b3b98cab29959fe722d687d4ecab
Carlo Pulcini <cpulcini2003@gmail.com>
2023-03-08 10:14:44 +0100

    c1

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu Arsoli!";
+		return "Greetings from Spring Boot + Tanzu Bologna!";
 	}
 
 }
